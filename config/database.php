92ae4eca474c8678dcd475447b7c702f52f1421f
shoven <441653192@qq.com>
2018-10-17 13:58:36 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'www.lazyor.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'cp'),
             'username' => env('DB_USERNAME', 'caipiao'),
